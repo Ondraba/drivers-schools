78beef273d147d2974d222cd5ab10c4d3493b27d
bejf <hbejvl@gmail.com>
2017-05-31 21:35:15 +0200

    added client section to the folder structure, created index page and contact page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'next/link'
+import Layout from '../components/layout/Layout'
+
+export default () => (
+  <Layout>
+    <h1>Index Page</h1>
+  </Layout>
+)
