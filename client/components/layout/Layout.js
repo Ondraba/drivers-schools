1477862099afbc8c6f98450cbe0fb95739deb9cc
bejf <hbejvl@gmail.com>
2017-09-27 00:16:04 +0200

    added fela styling to the project and declared some basic fela components, added basic styling to pages

--- client/components/layout/Layout.js
+++ client/components/layout/Layout.js
@@ -1,25 +1,46 @@
+import { Provider } from 'react-fela'
+import felaRenderer from '../../../lib/felaRenderer'
 import Link from 'next/link'
 import Head from 'next/head'
+import Body from '../fela/Body'
+import Container from '../fela/Container'
 
 export default({ children, title = 'Page Title' }) => (
-  <div>
-    <Head>
-      <title>{ title }</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-    <header>
-      <nav>
-        <Link href='/' prefetch><a>Home</a></Link> |
-        <Link href='/contact' prefetch><a>Contact</a></Link> |
-        <Link href='/articles' prefetch><a>Articles</a></Link>
-      </nav>
-    </header>
+  <Provider renderer={felaRenderer}>
+    <div>
+      <Head>
+        <title>{ title }</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <style jsx global>{`
+        body {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          font-family: 'Arial';
+        }
+      `}</style>
 
-    { children }
 
-    <footer>
-      {'I`m here to stay'}
-    </footer>
-  </div>
+      <Container>
+        <header>
+          <nav>
+            <Link href='/' prefetch><a>Home</a></Link> |
+            <Link href='/contact' prefetch><a>Contact</a></Link> |
+            <Link href='/articles' prefetch><a>Articles</a></Link> |
+            <Link href='/admin' prefetch><a>Admin</a></Link>
+          </nav>
+        </header>
+      </Container>
+      <Container>
+        { children }
+      </Container>
+      <Container>
+        <footer>
+          {'I`m here to stay'}
+        </footer>
+      </Container>
+    </div>
+  </Provider>
 )
